50d77c777c6c32a59b5016666959e2f5a9a45d16
Kadee80 <kaa328@nyu.edu>
2025-11-13 09:30:53 -0500

    Add final refactored working list wizard code

--- Week11/Redux_Toolkit/list-wizard-final/src/store/index.js
+++ Week11/Redux_Toolkit/list-wizard-final/src/store/index.js
@@ -1,62 +1,25 @@
-import {configureStore, createSlice} from '@reduxjs/toolkit'
-
-const movieSlice = createSlice({
-  name: 'movie',
-  initialState: [],
-  reducers: {
-    // name + '/' + functionName is how you access them
-    // action.type generated = 'song/addSong'
-    // this is the action.type that is generated in the behind the scenes actionCreator
-    addMovie(state, action) {
-      // redux toolkit uses immer library! thats why you can
-      // directly mutate state cringe
-      state.push(action.payload)
-    },
-    removeMovie(state, action) {
-      // action.payload is the name of the song we want to remove
-      // get the index of the song passed via payload
-      const index = state.indexOf(action.payload)
-      // Array.splice() - vanilla js command
-      state.splice(index, 1)
-    },
-  },
-})
-
-// think of a slice like a specific useReducer/reducer function
-const songSlice = createSlice({
-  name: 'song',
-  initialState: [],
-  reducers: {
-    // name + '/' + functionName is how you access them
-    // action.type generated = 'song/addSong'
-    // this is the action.type that is generated in the behind the scenes actionCreator
-    addSong(state, action) {
-      // redux toolkit uses immer library! thats why you can
-      // directly mutate state cringe
-      state.push(action.payload)
-    },
-    removeSong(state, action) {
-      // action.payload is the name of the song we want to remove
-      // get the index of the song passed via payload
-      const index = state.indexOf(action.payload)
-      // Array.splice() - vanilla js command
-      state.splice(index, 1)
-    },
-  },
-})
+import {configureStore} from '@reduxjs/toolkit'
+import {songReducer, addSong, removeSong} from './slices/songSlice'
+import {movieReducer, addMovie, removeMovie} from './slices/movieSlice'
+import {reset} from './actions'
 
 // this is where you add your slices by keyname to your applicaiton wide state/store
 const store = configureStore({
   reducer: {
-    songs: songSlice.reducer,
-    movies: movieSlice.reducer,
+    songs: songReducer,
+    movies: movieReducer,
   },
 })
+
+// export all imported actionCreators for all of store
+// import from indiviual slices and centrally export everything from index
+// why? good practice, and avoids circ dependencies
+export {store, reset, addSong, removeSong, addMovie, removeMovie}
 
 // here, once the slices have been combined into our store
 // each slice is a key, the values inside are updated by its reducers
 // songSlice think of state as all things songState
-const startingState = store.getState()
+/*const startingState = store.getState()
 console.log(JSON.stringify(startingState))
 
 // update our metaState aka store with dispatch
@@ -71,10 +34,4 @@ store.dispatch(movieSlice.actions.addMovie('Face Off'))
 console.log(JSON.stringify(store.getState()))
 
 console.log(movieSlice.actions)
-
-// make sure you export all the things you need
-// the compiled application state to access values elsewhere
-export {store}
-// detructure and export out the actions/function from the compiled songSlice.actions (should be named .actionCreators)
-export const {addSong, removeSong} = songSlice.actions
-export const {addMovie, removeMovie} = movieSlice.actions
+*/
